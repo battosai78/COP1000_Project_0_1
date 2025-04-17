8788e9b6aa09137b0c245edf97def123a0fdc910
battosai78 <diazluke91@gmail.com>
2025-04-16 21:05:17 -0400

    VehicleAdd

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 #Initialize Array
-AllowedVehiclesList = [ 'Ford F-150', 'Chevrolet Silverado', 'Tesla CyberTruck', 'Toyota Tundra', 'Nissan Titan' ]
+AllowedVehiclesList = open("AllowedVehiclesList.txt","a")
 #Initialize Input variable
 menuSelect = None
+newVehicle = None
 
 #Present Menu
 print("********************************\n"
-"AutoCountry Vehicle Finder v0.2\n"
+"AutoCountry Vehicle Finder v0.3\n"
 "********************************\n"
 "Please Enter the following number below from the following menu:\n\n"
 "1. PRINT all Authorized Vehicles\n"
 "2. SEARCH for Authorized Vehicle\n"
-"3. Exit")
+"3. ADD Authorized Vehicle\n"
+"4. Exit")
 
 #Get User Input
 menuSelect = input()
@@ -21,11 +23,14 @@ if int(menuSelect) == 1:
     for vehicle in AllowedVehiclesList:
         print(vehicle)
 elif int(menuSelect) == 2:
-    print("Please Enter the full Vehicle name: ")
-    vehicleSearch = input()
+    vehicleSearch = input("Please Enter the full Vehicle name: \n")
     if vehicleSearch in AllowedVehiclesList:
         print(vehicleSearch + " is an authorized vehicle")
     else:
         print(vehicleSearch + " is not an authorized vehicle, if you received this in error please check the spelling and try again")
 elif int(menuSelect) == 3:
+    newVehicle = input("Enter the full Vehicle name you would like to add: \n")
+    AllowedVehiclesList.write("\n" + newVehicle)
+elif int(menuSelect) == 4:
     print("Thank you for using the AutoCountry Vehicle Finder, good-bye!")
+AllowedVehiclesList.close
