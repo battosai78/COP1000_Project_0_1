a783916585bc64036ef3a6d623f50397eabf0a50
battosai78 <diazluke91@gmail.com>
2025-04-19 17:17:47 -0400

    v0.4

--- main.py
+++ main.py
@@ -2,23 +2,26 @@
 menuSelect = 0  
 newVehicle = None
 AllowedVehiclesList = None
-while int(menuSelect) != 4:
+removeVehicle = None
+confirmRemove = None
+while int(menuSelect) != 5:
     #Present Menu
     print("********************************\n"
-    "AutoCountry Vehicle Finder v0.3\n"
+    "AutoCountry Vehicle Finder v0.4\n"
     "********************************\n"
     "Please Enter the following number below from the following menu:\n\n"
     "1. PRINT all Authorized Vehicles\n"
     "2. SEARCH for Authorized Vehicle\n"
     "3. ADD Authorized Vehicle\n"
-    "4. Exit\n"
+    "4. DELETE Authorized Vehicle\n"
+    "5. Exit\n"
     "********************************\n")
     #Get User Input
     menuSelect = input()
     #Convert input to integer and evaluate
     if int(menuSelect) == 1:
         AllowedVehiclesList = open("AllowedVehiclesList.txt","r")
-        print("The AutoCountry sales manager has authorized the purchase and selling of the following vehicles: ")
+        print("The AutoCountry sales manager has authorized the purchase and selling of the following vehicles: \n")
         for vehicle in AllowedVehiclesList:
             print(vehicle)
         AllowedVehiclesList.close    
@@ -31,10 +34,23 @@ while int(menuSelect) != 4:
             print(vehicleSearch + " is not an authorized vehicle, if you received this in error please check the spelling and try again")
             AllowedVehiclesList.close
     elif int(menuSelect) == 3:
-        AllowedVehiclesList = open("AllowedVehiclesList.txt","w")
+        AllowedVehiclesList = open("AllowedVehiclesList.txt","a")
         newVehicle = input("Enter the full Vehicle name you would like to add: \n")
         AllowedVehiclesList.write("\n" + newVehicle)   
         print("You have added " + newVehicle + " as an authorized vehicle")
-        AllowedVehiclesList.close      
+        AllowedVehiclesList.close
+    elif int(menuSelect) == 4:
+        AllowedVehiclesList = open("AllowedVehiclesList.txt","r")
+        removeVehicle = input("Enter the full Vehicle name you would like to remove: \n")
+        lines = AllowedVehiclesList.readlines()
+        confirmRemove = input("Are you sure you want to remove " + removeVehicle + " from the Authorized Vehicle List?\n")
+        if confirmRemove == "yes":
+            AllowedVehiclesList = open("AllowedVehiclesList.txt","w")
+            for line in lines:
+                if line != removeVehicle:
+                    AllowedVehiclesList.write(line)
+        else:
+            continue
+        print("You have REMOVED " + removeVehicle + " as an authorized vehicle")   
 print("Thank you for using the AutoCountry Vehicle Finder, good-bye!")
 exit
